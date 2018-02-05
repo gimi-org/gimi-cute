23c103415f31f582313b929cc28fd387adc224ae
Mathias <mathias@barnpengar.se>
2018-02-05 14:21:27 +0100

    STOMP maybe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, {Component} from 'react'
 import CuteNotification from './components/CuteNotification'
 import moment from 'moment'
+import SockJsClient from 'react-stomp'
 
 type Props = {}
 type State = {
   notifications: Object[]
 }
 
+const webhookUrl = 'https://spirited-lizard.rmq.cloudamqp.com'
+
 export default class App extends Component<Props, State> {
   state = {notifications: []}
-
-  componentWillMount () {
-    // setInterval(this.getNotifications, 1000)
-    this.getNotifications()
-  }
 
   render() {
     var {notifications} = this.state
     return (
       <div style={styles.container} className="App">
+      <SockJsClient url={webhookUrl} headers={this.getHeaders()} topics={['/topic/cute-feed']} onMessage={this.onNotification} onConnect={(res) => console.log(res)} />
         <h1>Gimi cute</h1>
         {notifications.length > 0 ? notifications.map(this.renderNotifications) : <div />}
       </div>
@@ -29,11 +28,17 @@ export default class App extends Component<Props, State> {
     return <CuteNotification text={notif.text} date={notif.date} key={index} />
   }
 
-  getNotifications = () => {
-    var {notifications} = this.state
-    var notification = {text: 'Hello im testin', date: moment()}
-    notifications.unshift(notification)
-    this.setState({notifications})
+  getHeaders = () => {
+    return {
+      port: 61613,
+      login: 'stomp',
+      passcode: 'd5wjJX2TBJ1nC0nFyyKj',
+    }
+  }
+
+  onNotification = (notification: Object) => {
+    alert('notif')
+    console.log(notification)
   }
 }
 
